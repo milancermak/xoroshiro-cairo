b588698447beb871c01964a816f015b0affa9db3
Milan Cermak <milan.cermak@gmail.com>
2022-02-14 11:56:29 +0100

    feat: xoroshiro128**

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # xoroshiro-cairo
-xoroshiro PRNG implementation in Cairo
+
+A [xoroshiro128**](https://prng.di.unimi.it/) pseudorandom number generator implementation in Cairo.
+
+## Deploying
+
+Presuming you have [nile](https://github.com/OpenZeppelin/nile) installed, you can `compile` and `deploy` the contract. You'll need a single number (a seed) that's passed in as an input argument to the constructor:
+
+```sh
+nile compile
+nile deploy --network goerli xoroshiro128_starstar 161803398
+```
+
+An instance of this contract has been deployed at [`0x06c4cab9afab0ce564c45e85fe9a7aa7e655a7e0fd53b7aea732814f3a64fbee`](https://goerli.voyager.online/contract/0x06c4cab9afab0ce564c45e85fe9a7aa7e655a7e0fd53b7aea732814f3a64fbee#transactions), you can use it as well.
+
+## Usage
+
+You can probe the deployed contract on Goerli for a random number inside your Starknet contract:
+
+```
+%lang starknet
+
+const XOROSHIRO_ADDR = 0x06c4cab9afab0ce564c45e85fe9a7aa7e655a7e0fd53b7aea732814f3a64fbee
+
+@contract_interface
+namespace IXoroshiro:
+    func next() -> (rnd : felt):
+    end
+end
+
+@external
+func get_next_rnd{syscall_ptr : felt*, range_check_ptr}() -> (rnd : felt):
+    let (rnd) = IXoroshiro.next(contract_address=XOROSHIRO_ADDR)
+    return (rnd)
+end
+```
+
+The contract has not been audited, use it at your own discresion.
---
+++ tests/test_xoroshiro128_starstar.py
@@ -0,0 +1,75 @@
+import pytest
+
+U64 = 2**64-1
+STATE = (0, 0)
+
+@pytest.mark.asyncio
+async def test_next(x128_ss):
+
+    s0 = splitmix64(42)
+    s1 = splitmix64(s0)
+    global STATE
+    STATE = (s0, s1)
+
+    def rotl(x, k):
+        return (x << k) | (x >> (64 - k))
+
+    def next():
+        global STATE
+        s0, s1 = STATE
+        result = (rotl(s0 * 5, 7) * 9) & U64
+
+        s1 ^= s0
+        new_s0 = (rotl(s0, 24) ^ s1 ^ (s1 << 16)) & U64
+        new_s1 = (rotl(s1, 37)) & U64
+        STATE = (new_s0, new_s1)
+        return result
+
+    for r in range(1000):
+        tx = await x128_ss.next().invoke()
+        r = next()
+        assert tx.result.rnd == r
+
+
+@pytest.mark.asyncio
+async def test_rotl(x128_ss_test):
+    for (x, k) in [(1, 0), (1, 1), (2**64, 63), (2**64, 64), (2**123, 64)]:
+        tx = await x128_ss_test.test_rotl(x, k).call()
+        r = (x << k) | (x >> (64 - k))
+        assert tx.result.out == r
+
+
+# https://xoshiro.di.unimi.it/splitmix64.c
+def splitmix64(x):
+    U64 = 2**64-1
+
+    z = x + 0x9e3779b97f4a7c15
+    z &= U64
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9
+    z &= U64
+    z = (z ^ (z >> 27)) * 0x94d049bb133111eb
+    z &= U64
+    return (z ^ (z >> 31)) & U64
+
+
+@pytest.mark.asyncio
+async def test_splitmix64(x128_ss_test):
+    for x in (0, 1, 2**64-1):
+        tx = await x128_ss_test.test_splitmix64(x).call()
+        assert tx.result.out == splitmix64(x)
+
+
+@pytest.mark.asyncio
+async def test_rshift(x128_ss_test):
+    test_cases = [
+        (1, 0),
+        (1, 1),
+        (2**127, 20),
+        (2**128+1, 31),
+        (2**128+1, 32),
+        (2**192+2**30, 45),
+        (2**250+2**20, 123)
+    ]
+    for (v, b) in test_cases:
+        tx = await x128_ss_test.test_rshift(v, b).call()
+        assert tx.result.out == v >> b
